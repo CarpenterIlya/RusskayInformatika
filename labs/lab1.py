4ec14229f03bedf6ddd75bda5d68884f012044c3
CarpenterIlya <144203292+CarpenterIlya@users.noreply.github.com>
2023-09-27 11:05:54 +0500

    Update lab1.py
    Эксперементально добавил задачу 4

--- labs/lab1.py
+++ labs/lab1.py
@@ -4,17 +4,20 @@ while True:
     print()
     if SAD == 1: #Надо же как то определить, какое задание ...
         x, y, z = map(float,input("Введите x y z соответственно: ").split())
-        print("a =", (x ** 0.5 - (abs(y)) ** 0.25)/(x - math.log2(y)))
-        print("b =", math.tan((x - 1) ** 0.5) - (abs(z)) ** (1 / 3) - y ** (1 / 6) + x)
+        print("a =", (x ** 0.5 - (abs(y)) ** 0.25)/(x - math.log2(y)))   #вычислил и ввёл
+        print("b =", math.tan((x - 1) ** 0.5) - (abs(z)) ** (1 / 3) - y ** (1 / 6) + x)  #вычислил и ввёл
         print()
     elif SAD == 2:
         x = float(input("Введите значение x: "))
-        print("f(x) = ", x ** 2 + (-5 * x) ** 0.5 / (2 * x + 5))
+        print("f(x) = ", x ** 2 + (-5 * x) ** 0.5 / (2 * x + 5)) #вычислил и ввёл
         print()
     elif SAD == 3:
         x = float(input("Введите значение x: "))
-        print("f(x) = ", (math.cos(math.sin(1 / x ** 2))) ** 2)
+        print("f(x) = ", (math.cos(math.sin(1 / x ** 2))) ** 2)   #вычислил и ввёл
         print() #для эстетичности
+    elif SAD == 4:
+        k , A , B = map(float,input("Введите k A B (A и B в градусах), в соответствующем порядке: ").split())
+        print("S =", k ** 2 * (math.sin(A) * math.sin(B)) / (2 * math.sin(A + B))
     exit = float(input("введите 1 для выхода: "))
     if exit == 1:
         break
