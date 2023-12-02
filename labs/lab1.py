e9f9817976d6405f7822a63d87c51dc127034cc9
CarpenterIlya <144203292+CarpenterIlya@users.noreply.github.com>
2023-12-02 16:16:59 +0500

    Update lab1.py
    Завершил работу над этой лабой. Нужно доделать одно задание

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,61 +1,4 @@
-import math                                         #импортировал математику
-while True:
-    while True:
-        SaD = float(input("Введите номер задания: "))     #вводим номер нужного задания
-        if SaD // 1 != 0:
-            print("Вводи целое число!")                    #а вдруг глупый пользователь
-        elif SaD > 9 :
-            print("Такого номера нет!")                     #всяко бывает
-        else :
-            break
-    print("")
-    if SaD == 1:                                    #Надо же как то определить, какое задание ...
-        x, y, z = map(float,input("Введите значения x y z соответственно: ").split())            #вводим
-        print("a =", ((abs(x - 1)) ** (1 / 3) + math.cos(y))/(math.tan(y) + math.sin(y)))      #вычислил и ввёл
-        print("b =", (math.log((abs(z - 1)) ** 0.5)) + (abs(y) ** (1/3)/((1 + z ** 2) ** 0.5) + math.sin(y) * math.sin(x))                              #вычислил и ввёл
-    if SaD == 2:
-        x = float(input("Введите значение x: "))                                          #вводим
-        print("f(x) = ", ((x ** 2 + 2 * x) / b - 1 * x ** 2) ** 0.5)                         #вычислил и ввёл
-
-    if SaD == 3:
-        x = float(input("Введите значение x: "))                                            #вводим
-        print("f(x) = ", (math.tan(x)) ** 2 * abs(math.log(x ** 2)))                         #вычислил и ввёл
-                                                                           #для эстетичности
-    if SaD == 4:
-        x1 , y1 = map(float,input("Введите координаты первой точки (x, y): ").split())            #вводим
-        x2 , y2 = map(float,input("Введите координаты второй точки: ").split())                    #вводим
-        x3 , y3 = map(float,input("Введите координаты третьей точки: ").split())                    #вводим
-        AB = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-        BC = ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5
-        AC = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5                                            #вычислил длины сторон
-        if AB == BC :
-            h = (AB ** 2 - (AC / 2) ** 2) ** 0.5
-            print("S =", h * AC )
-        elif BC == AC :
-            h = (BC ** 2 - (AB / 2) ** 2) ** 0.5
-            print("S =", h * AB )
-        elif AC == AB :
-            h = (AC ** 2 - (BC / 2) ** 2) ** 0.5
-            print("S =", h * BC )
-        else:
-            h = (AB ** 2 - (AC / 2) ** 2) ** 0.5
-            print("S =", h * AC )
-        print("P =", (((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5 + ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5) * 2)
-        print("")
-    if SaD == 5:
-        m1 , m2 = map(float,input("Введите массы двух тел: ").split())             #вводим
-        r = float(input("Введите расстояние между телами: "))                #вводим
-        print("F = ", G * m1 * m2 / r)
-
-    if SaD == 6:
-        print("")
-    exit = float(input("введите 1 для выхода: "))
-    if exit == 1:
-        break
-
-
 import math  # импортировал математику
-
 while True:
     while True:
         SaD = float(input("Введите номер задания (0 - выход): "))  # вводим номер нужного задания
@@ -98,11 +41,30 @@ while True:
         print("R = ", ( ((p - ab)(p - bc)(p - ca)) / p ) ** 0.5)
         print("")
     if SaD == 7:
-        A1, B1, C1 = map()
-        print("")
+        #A1, B1, C1 = map(float, input("Введите A1, B1, C1: ").split())
+        #A2, B2, C2 = map(float, input("Введите A2, B2, C2: ").split())
+        #D = A1 * B2 - A2 * B1
+        #x = (C1 * B2 - C2 * B1) / D
+        #y = (A1 * C2 - A2 * C1) / D
+        #print("Решение системы = ", A1 * x + B1 * y )
+        #print("")
+        #
+        #
+        #
+        #
+        #
     if SaD == 8:
+        V1, V2, S1, T = map(float, input("Введите значения V1, V2, S1 и T, через пробел: ").split())
+        while True:
+            if V1 < 1 or V1 > 100 or V2 < 1 or V2 > 100 or S1 < 1 or S1 > 100 or T < 1 or T > 100
+                print("Каждое из значений лежит от 1 до 100 включительно!")
+            else:
+                break
+        print("S = ", S1 + V1 * T + V2 * T)
         print("")
     if SaD == 9:
+        part_first, part_second, profit = map(float, input("Введите значения вложений двух предпринимателей и их доход: ").split())
+        print("Прибыль первого: ", profit * part_first / (part_first + part_second) ,"Прибыль второго: ", profit * part_second / (part_first + part_second))
         print("")
     if SaD == 0:
         break
