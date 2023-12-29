c464addc155932ddb7c3638e8301b21f2cf98386
CarpenterIlya <144203292+CarpenterIlya@users.noreply.github.com>
2023-12-29 15:55:09 +0500

    Create lab3.py
    загрузил заранее созданную недоделку

---
+++ labs/lab3.py
@@ -0,0 +1,48 @@
+def print_char_list(array):
+
+def proverka1(array, symbol):
+
+def proverka2(array, first_symbol, second_symbol):
+
+def proverka3(array):
+
+def proverka4(array):
+
+def proverka5(array):
+
+def print_char_2d_array(array)
+
+def vistrel(row, column, array)
+
+def task_1()
+
+def task_2()
+    a = int(input("Введите размер стороны поля для игры Морской бой: "))
+    mas = [[.] * a for i in range (a)]
+    while True:
+	
+
+def task_3()
+    for i in range(5):
+	s = open("DLYA_task_3.txt", "a")
+    file.close()
+
+def task_4()
+
+def task_5()
+    stop = input("Введите стоп слово: ")
+    while True:
+	U = input("Введите текст: ")
+	u = U[0]
+	if U == stop
+	elif ord(u) in [1030, 1064]:
+	    U = open("DLYA_task_5-1.txt", "a")
+	else:
+	    U = open("DLYA_task_5-2.txt", "a")
+    file.close()
+
+def task_6()
+
+def task_7()
+
+def task_8()
